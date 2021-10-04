9c1074e5b5e0f4e60e8eec090c59d110bb3f8e55
Soichi Takamura <thepiglovesyou@gmail.com>
2021-10-04 16:43:56 +0900

    fix: Tailwind CSS Purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  purge: isProd
-    ? ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}']
-    : undefined,
-  darkMode: false, // or 'media' or 'class'
+  purge: isProd ? ['./src/**/*.{js,ts,jsx,tsx}'] : undefined,
+  darkMode: false,
   theme: {
     extend: {},
   },
